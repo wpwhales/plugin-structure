c6d0102b41e4c3890dfd9a4e719c67e7a41f2a69
Hamza Mirza <hamza.tariq.mirza@gmail.com>
2024-03-26 03:25:22 +0500

    Seeder command class DatabaseSeeder is working

--- src/Core/Database/Console/Seeds/SeedCommand.php
+++ src/Core/Database/Console/Seeds/SeedCommand.php
@@ -132,7 +132,7 @@ class SeedCommand extends Command
     protected function getOptions()
     {
         return [
-            ['class', null, InputOption::VALUE_OPTIONAL, 'The class name of the root seeder', 'Database\\Seeders\\DatabaseSeeder'],
+            ['class', null, InputOption::VALUE_OPTIONAL, 'The class name of the root seeder', 'WPWCore\\Database\\Seeders\\DatabaseSeeder'],
             ['database', null, InputOption::VALUE_OPTIONAL, 'The database connection to seed'],
             ['force', null, InputOption::VALUE_NONE, 'Force the operation to run when in production'],
         ];
