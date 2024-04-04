31c66a5b6336f798355178309798e44edacb2535
Hamza Mirza <hamza.tariq.mirza@gmail.com>
2024-04-05 01:28:29 +0500

    scheduler path registered

--- src/Core/Concerns/RegisterActionScheduler.php
+++ src/Core/Concerns/RegisterActionScheduler.php
@@ -14,12 +14,16 @@ trait RegisterActionScheduler
         $path = dirname(__FILE__, 4);
         if (file_exists($path . "/woocommerce/action-scheduler/action-scheduler.php")) {
             require $path . "/woocommerce/action-scheduler/action-scheduler.php";
-        } else
-            if (file_exists($path . "/vendor/woocommerce/action-scheduler/action-scheduler.php")) {
-                require $path . "/vendor/woocommerce/action-scheduler/action-scheduler.php";
-            } else {
-                throw new \Exception("Please install the action scheduler . composer require woocommerce/action-scheduler");
-            }
+        } else if (file_exists($path . "/vendor/woocommerce/action-scheduler/action-scheduler.php")) {
+            require $path . "/vendor/woocommerce/action-scheduler/action-scheduler.php";
+        } else if(file_exists($this->basePath("vendor/woocommerce/action-scheduler/action-scheduler.php"))) {
+            require $this->basePath("vendor/woocommerce/action-scheduler/action-scheduler.php");
+
+        } else {
+
+            throw new \Exception("Please install the action scheduler . composer require woocommerce/action-scheduler");
+        }
+
 
         $this->app->singleton(\ActionScheduler::class, function ($app) {
             return new \WPWCore\ActionScheduler\ActionScheduler();
