6638f688d2a885c893bc3fdee14e71af826b477c
Hamza Mirza <hamza.tariq.mirza@gmail.com>
2024-08-20 19:40:34 +0500

    create test case from command lines

--- src/Core/Console/Commands/TestMakeCommand.php
+++ src/Core/Console/Commands/TestMakeCommand.php
@@ -3,11 +3,12 @@
 namespace WPWCore\Console\Commands;
 
 use WPWCore\Console\GeneratorCommand;
-use Illuminate\Support\Str;
+use WPWhales\Support\Str;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use function WPWCore\base_path;
 
 #[AsCommand(name: 'make:test')]
 class TestMakeCommand extends GeneratorCommand
@@ -40,52 +41,55 @@ class TestMakeCommand extends GeneratorCommand
      */
     protected function getStub()
     {
-        $suffix = $this->option('unit') ? '.unit.stub' : '.stub';
+        if ($this->option('unit')) {
+            $name = "test.unit.stub";
+        } elseif ($this->option('wordpress')) {
+            $name = "test-wordpress.stub";
+        } elseif ($this->option('wordpress-ajax')) {
+            $name = "test-wordpress-ajax.stub";
+        } else {
+            $name = "test-wordpress.stub";
+        }
 
-        return $this->option('pest')
-            ? $this->resolveStubPath('/stubs/pest'.$suffix)
-            : $this->resolveStubPath('/stubs/test'.$suffix);
+        return $this->resolveStubPath('/stubs/' . $name);
+
     }
 
     /**
      * Resolve the fully-qualified path to the stub.
      *
-     * @param  string  $stub
+     * @param string $stub
      * @return string
      */
     protected function resolveStubPath($stub)
     {
         return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
-                        ? $customPath
-                        : __DIR__.$stub;
+            ? $customPath
+            : __DIR__ . $stub;
     }
 
     /**
      * Get the destination class path.
      *
-     * @param  string  $name
+     * @param string $name
      * @return string
      */
     protected function getPath($name)
     {
         $name = Str::replaceFirst($this->rootNamespace(), '', $name);
 
-        return base_path('tests').str_replace('\\', '/', $name).'.php';
+        return base_path('tests/phpunit/testcases') . str_replace('\\', '/', $name) . '.php';
     }
 
     /**
      * Get the default namespace for the class.
      *
-     * @param  string  $rootNamespace
+     * @param string $rootNamespace
      * @return string
      */
     protected function getDefaultNamespace($rootNamespace)
     {
-        if ($this->option('unit')) {
-            return $rootNamespace.'\Unit';
-        } else {
-            return $rootNamespace.'\Feature';
-        }
+        return $rootNamespace;
     }
 
     /**
@@ -108,15 +112,16 @@ class TestMakeCommand extends GeneratorCommand
         return [
             ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the test already exists'],
             ['unit', 'u', InputOption::VALUE_NONE, 'Create a unit test'],
-            ['pest', 'p', InputOption::VALUE_NONE, 'Create a Pest test'],
+            ['wordpress', 'w', InputOption::VALUE_NONE, 'Create a Wordpress test'],
+            ['wordpress-ajax', 'wa', InputOption::VALUE_NONE, 'Create a Ajax Wordpress test'],
         ];
     }
 
     /**
      * Interact further with the user if they were prompted for missing arguments.
      *
-     * @param  \Symfony\Component\Console\Input\InputInterface  $input
-     * @param  \Symfony\Component\Console\Output\OutputInterface  $output
+     * @param \Symfony\Component\Console\Input\InputInterface $input
+     * @param \Symfony\Component\Console\Output\OutputInterface $output
      * @return void
      */
     protected function afterPromptingForMissingArguments(InputInterface $input, OutputInterface $output)
@@ -126,17 +131,16 @@ class TestMakeCommand extends GeneratorCommand
         }
 
         $type = $this->components->choice('Which type of test would you like', [
-            'feature',
             'unit',
-            'pest feature',
-            'pest unit',
-        ], default: 0);
+            'wordpress',
+            'wordpress ajax',
+        ], default: 1);
 
         match ($type) {
-            'feature' => null,
+
             'unit' => $input->setOption('unit', true),
-            'pest feature' => $input->setOption('pest', true),
-            'pest unit' => tap($input)->setOption('pest', true)->setOption('unit', true),
+            'wordpress' => $input->setOption('wordpress', true),
+            'wordpress ajax' => $input->setOption('wordpress-ajax', true),
         };
     }
 }
